223f48e1ea9d8b9974c4f396f2757991ee545061
Swapnil Katwe <swapnil.katwe111@gmail.com>
2024-09-15 20:16:20 +0200

    Initial React app setup, Header footer and note classes add.

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+    // var year = Date().get
+    return(
+        <div>
+            <footer>
+                <p>Copyright 2024 </p>
+            </footer>
+            </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <header>Keeper</header>
+        </div>
+    );
+}
+
+export default Header;
